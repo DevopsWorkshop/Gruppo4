eed8a72e9021cffc077b7ed4536337ce2528a4be
wssys <wssys@webscience.it>
2017-09-14 14:11:34 +0000

    0

--- src/main/java/mypackage/Factor.java
+++ src/main/java/mypackage/Factor.java
@@ -3,7 +3,7 @@ package mypackage;
 public class Factor {
 public static int factor (int valore)
 	{
-	if (valore<=0) return 1;
+	if (valore<=0) return 0;
 	else return valore * factor(valore - 1);
 	}
 }
